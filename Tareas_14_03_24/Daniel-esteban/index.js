d9e5483af62b4279f07d146cd542b5580a5e24f8
DanielestebanLL <danielestebanll@icloud.com>
2024-03-18 19:02:33 -0500

    tarea(6)-Daniel.esteban.14.03.2024

---
+++ Tareas_14_03_24/Daniel-esteban/index.js
@@ -0,0 +1,20 @@
+function bAD(binario) {
+    let decimal = 0;
+    const longitud = binario.length;
+
+    for (let i = 0; i < longitud; i++) {
+        const bit = binario[longitud - i - 1];
+        if (bit === '1') {
+            decimal += Math.pow(2, i);
+        } else if (bit !== '0') {
+            
+            return NaN;
+        }
+    }
+
+    return decimal;
+}
+
+const numeroBinario = "101010";
+const numeroDecimal = bAD(numeroBinario);
+console.log(numeroDecimal); 
