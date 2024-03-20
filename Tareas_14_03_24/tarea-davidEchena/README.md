6903138b9a845c8d47a27bbfe6ae2b02f6be3bf7
David Echenagucia <davidleonardo220@gmail.com>
2024-03-20 10:25:12 -0500

    tarea(6): davidechena-14-03-24

---
+++ Tareas_14_03_24/tarea-davidEchena/README.md
@@ -0,0 +1,6 @@
+# Tarea
+
+- Se hcieron los codigos correspondientes a:
+
+- Escribir en consola un numero **binario** y se regresa un numero **decimal**.
+- Escribir en consola un numero **decimal** y se regresa un numero **binario**.
---
+++ Tareas_14_03_24/tarea-davidEchena/convertirDeBinarioADecimal.js
@@ -0,0 +1,21 @@
+const rli = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const convertirABinario = (num) => {
+  num_array = num.split("").reverse();
+  num_decimal = 0;
+  for (let i = 0; i < num.length; i++) {
+    if (num_array[i] == "1") {
+      num_decimal += Math.pow(2, i);
+    }
+  }
+  console.log(num_decimal);
+};
+
+rli.question("Di un numero binario: ", (num) => {
+  convertirABinario(num);
+
+  rli.close();
+});
