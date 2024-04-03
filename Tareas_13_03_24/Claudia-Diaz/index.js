dc86b76b6091f47fd148cf70f545ab72e8ad9507
claudiadiaz <claudiadiazg@creasistemas.com>
2024-04-02 20:15:34 -0500

    tarea 5

--- Tareas_13_03_24/Claudia-Diaz/index.js
+++ Tareas_13_03_24/Claudia-Diaz/index.js
@@ -1,3 +1,12 @@
-console.log-tarea5
-numeros-primos 
-claudia-Diaz 
+for (let i = 2; i <= 20; i++) {  
+    const divisor = []; 
+  
+    for (let j = 1; j <= i; j++) { 
+      if (i % j === 0) {  
+        divisor.push(j);  
+      }
+    }
+    if (divisor.length === 2) { 
+      console.log(`El numero ${i} es primo`);  
+    }
+  }
